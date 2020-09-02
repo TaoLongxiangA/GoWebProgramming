b916464ccc8fbad5f09f2485682379d3016a94b8
taolx <taolx25@foxmail.com>
2020-09-02 20:56:23 +0800

    initial commit

--- ch4/example4_4/main.go
+++ ch4/example4_4/main.go
@@ -6,16 +6,29 @@ import (
 )
 
 func process(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	fmt.Fprintln(w, r.Form)
-	fmt.Fprintln(w, r.PostForm)
-	fmt.Fprintln(w, "next line is MultipartForm")
-	r.ParseMultipartForm(1024)
-	fmt.Fprintln(w, r.MultipartForm)
+	//log.Println(len(r.Form))
+	//log.Println(len(r.PostForm))
+	//fmt.Fprintln(w, r.Form)
+	//fmt.Fprintln(w, r.PostForm)
+	//r.ParseForm()
+	//log.Println(len(r.Form))
+	//log.Println(len(r.PostForm))
+	//fmt.Fprintln(w, r.Form)
+	//fmt.Fprintln(w, r.PostForm)
+
+	fmt.Fprintln(w, r.PostFormValue("hello"))
+	fmt.Fprintln(w, r.PostFormValue("post"))
+
+	fmt.Fprintln(w, r.FormValue("hello"))
+	fmt.Fprintln(w, r.FormValue("post"))
+
+	//fmt.Fprintln(w, "next line is MultipartForm")
+	//r.ParseMultipartForm(1024)
+	//fmt.Fprintln(w, r.MultipartForm)
 }
 
 func main() {
-	server := http.Server{Addr: "127.0.0.1:8080"}
+	server := http.Server{Addr: "127.0.0.1:8081"}
 	http.HandleFunc("/process", process)
 	server.ListenAndServe()
 }
